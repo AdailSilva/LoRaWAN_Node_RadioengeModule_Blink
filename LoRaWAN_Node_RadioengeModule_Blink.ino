6d757a6090886a9b982788ff9e66788bdbfa0c60
Adail Silva <adail@bdmg.mg.gov.br>
2021-11-15 11:03:15 -0300

    Initial commit

---
+++ LoRaWAN_Node_RadioengeModule_Blink.ino
@@ -0,0 +1,128 @@
+/* 
+ *   
+ *  Project:          IoT Energy Meter with C/C++, Java/Spring, TypeScript/Angular and Dart/Flutter;
+ *  About:            End-to-end implementation of a LoRaWAN network for monitoring electrical quantities;
+ *  Version:          1.0;
+ *  Backend Mote:     ATmega328P/ESP32/ESP8266/ESP8285/STM32;
+ *  Radios:           RFM95w and LoRaWAN EndDevice Radioenge Module: RD49C;
+ *  Sensors:          Peacefair PZEM-004T 3.0 Version TTL-RTU kWh Meter;
+ *  Backend API:      Java with Framework: Spring Boot;
+ *  LoRaWAN Stack - Customized for the Frequency Plan adopted in Brazil for LoRaWAN --> [AU915]:
+ *  MCCI Arduino LoRaWAN Library (LMiC: LoRaWAN-MAC-in-C) version 3.0.99;
+ *  
+ *  Or:
+ *  LoRaWAN Stack - Implementation approved by the National Telecommunications Agency of Brazil
+ *  and inserted by the company Radioenge:
+ *  LoRaMac-node Library (LoRaWAN L2 1.0.3 - Released / API via AT commands);
+ *  
+ *  Activation mode:  Activation by Personalization (ABP) or Over-the-Air Activation (OTAA);
+ *  Author:           Adail dos Santos Silva
+ *  E-mail:           adail101@hotmail.com
+ *  WhatsApp:         +55 89 9 9433-7661
+ *  
+ *  This project was conceived using the MCCI Arduino LoRaWAN Library (LMiC: LoRaWAN-MAC-in-C) version 3.0.99,
+ *  This RD49C module in turn has the copyright belonging to the manufacturer, the company Radioenge do Brasil.
+ *  All remaining implementation is authored by the creator of the LoRaWAN Electricity Meter project.
+ *  
+ *  WARNINGS:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the “Software”), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *  the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *  
+ */
+
+
+/*
+ *  Radioenge Module - Blink Example
+ */
+
+/********************************************************************
+ _____              __ _                       _   _             
+/  __ \            / _(_)                     | | (_)            
+| /  \/ ___  _ __ | |_ _  __ _ _   _ _ __ __ _| |_ _  ___  _ __  
+| |    / _ \| '_ \|  _| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \ 
+| \__/\ (_) | | | | | | | (_| | |_| | | | (_| | |_| | (_) | | | |
+ \____/\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
+                          __/ |                                  
+                         |___/                                   
+********************************************************************/
+
+/* Includes */
+
+/* Definitions */
+
+// Pinout - IOs:
+#define LED_RED                 PB5
+#define LED_GREEN               PB8
+#define LED_YELLOW              PB9
+
+
+/*****************************
+ _____           _      
+/  __ \         | |     
+| /  \/ ___   __| | ___ 
+| |    / _ \ / _` |/ _ \
+| \__/\ (_) | (_| |  __/
+ \____/\___/ \__,_|\___|
+*****************************/
+
+/*****************************
+ _____      _               
+/  ___|    | |              
+\ `--.  ___| |_ _   _ _ __  
+ `--. \/ _ \ __| | | | '_ \ 
+/\__/ /  __/ |_| |_| | |_) |
+\____/ \___|\__|\__,_| .__/ 
+                     | |    
+                     |_|    
+******************************/
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println();
+  
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);  
+
+}
+
+/*****************************
+ _                       
+| |                      
+| |     ___   ___  _ __  
+| |    / _ \ / _ \| '_ \ 
+| |___| (_) | (_) | |_) |
+\_____/\___/ \___/| .__/ 
+                  | |    
+                  |_|    
+*****************************/
+
+void loop() {
+  digitalWrite(LED_RED, LOW);
+  delay(250);
+  digitalWrite(LED_RED, HIGH);
+  delay(1000);
+  digitalWrite(LED_GREEN, LOW);
+  delay(250);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(1000);
+  digitalWrite(LED_YELLOW, LOW);
+  delay(250);
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(1000);
+}
